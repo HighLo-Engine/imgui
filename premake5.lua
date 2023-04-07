5cfb7177a6951ee9a678c628fe53593dae32f179
Cankar001 <Info.Can-Karka@Web.de>
2023-04-07 11:17:22 +0200

    updated imgui

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+    staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
@@ -12,6 +13,7 @@ project "ImGui"
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
@@ -22,20 +24,75 @@ project "ImGui"
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
 
-	filter "configurations:Debug"
+	filter "configurations:Debug-OpenGL"
 		runtime "Debug"
 		symbols "on"
 
-	filter "configurations:Release"
+	filter "configurations:Release-OpenGL"
 		runtime "Release"
 		optimize "on"
+
+    filter "configurations:Dist-OpenGL"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
+
+	filter "configurations:Debug-Vulkan"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release-Vulkan"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist-Vulkan"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
 		
+	filter "configurations:Debug-DX11"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release-DX11"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist-DX11"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
 		
+	filter "configurations:Debug-DX12"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release-DX12"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist-DX12"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
+		
+	filter "configurations:Debug-Metal"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release-Metal"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist-Metal"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
+		
+	
